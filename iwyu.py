5fff44c4807fb7330cee17cf8532b07153db3802
Orson Peters <orsonpeters@gmail.com>
2015-11-23 13:50:21 +0100

    Lots of names added to header_database.txt and proper lexing using PLY.

--- iwyu.py
+++ iwyu.py
@@ -4,6 +4,8 @@ import sys
 import re
 import os
 import readline # NOQA
+import ply.lex
+import ply.cpp
 
 # Python 2-3 compatability.
 try:
@@ -23,7 +25,6 @@ except AttributeError:
 IWYU_PATH = os.path.dirname(os.path.realpath(__file__))
 VALID_WILDCARD = re.compile(r"(?:[*a-zA-Z_][*a-zA-Z0-9_]*\s*::\s*)*[*a-zA-Z_][*a-zA-Z0-9_]*")
 IDENTIFIERS = re.compile(r"\b(?:[a-zA-Z_][a-zA-Z0-9_]*\s*::\s*)*[a-zA-Z_][a-zA-Z0-9_]*")
-STRIP_WS = re.compile(r"\s*")
 
 
 def validate_wildcard(wildcard, linenr=0):
@@ -66,7 +67,7 @@ def store_database(headers, wildcards):
     dst = os.path.join(IWYU_PATH, "header_database.txt")
 
     with open(tmp, "w") as db:
-        wildcards.sort(key=lambda w: w[0] != "?")
+        wildcards = sorted(wildcards, key=lambda w: w[0] != "?")
         for w in wildcards:
             if w[0] in "?!":
                 db.write("{} {}\n".format(w[0], w[1].replace('.*', '*')))
@@ -79,26 +80,55 @@ def store_database(headers, wildcards):
     os_replace(tmp, dst)
 
 
+def get_identifiers(code):
+    lex = ply.lex.lex(module=ply.cpp)
+    lex.input(ply.cpp.trigraph(data))
+
+    tokens = list(lex)
+    i = 0
+    while i < len(tokens):
+        if tokens[i].type != "CPP_ID":
+            i += 1
+            continue
+
+        ident = tokens[i].value
+        i += 1
+        while "".join(t.type for t in tokens[i:i+3]) == "::CPP_ID":
+            ident += "::" + tokens[i+2].value
+            i += 3
+
+        yield ident
+
+
 if len(sys.argv) <= 1:
     print("Usage: {} <file>...".format(sys.argv[0]))
     sys.exit(0)
 
 headers, wildcards = load_database()
+store_database(headers, wildcards)
 wildcards_regex = make_wildcards_regex(wildcards)
+
 
 for filename in sys.argv[1:]:
     with open(filename) as f:
         data = f.read()
 
+    print("Includes for {}:".format(filename))
     headers_needed = set()
-    for identifier in re.findall(IDENTIFIERS, data):
-        identifier = re.sub(STRIP_WS, "", identifier)
+    identifiers_handled = set()
+
+    for identifier in get_identifiers(data):
+        if identifier in identifiers_handled:
+            continue
+
         if identifier in headers:
             headers_needed.add(headers[identifier])
+            identifiers_handled.add(identifier)
             continue
 
         m = re.match(wildcards_regex, identifier)
         if m:
+            identifiers_handled.add(identifier)
             wildcard = wildcards[m.lastindex - 1]
             if wildcard[0] == "!":
                 continue
@@ -108,7 +138,7 @@ for filename in sys.argv[1:]:
                 sys.stderr.write(identifier + ": ")
                 header = input().strip()
                 if header == "!":
-                    wildcards.append(["!", header])
+                    wildcards.append(["!", identifier])
                     wildcards_regex = make_wildcards_regex(wildcards)
                 else:
                     headers[identifier] = header
@@ -116,10 +146,10 @@ for filename in sys.argv[1:]:
 
                 store_database(headers, wildcards)
 
-    print("Includes for {}:".format(filename))
     # Key is a hack to make " sort after <.
     for header_needed in sorted(headers_needed, key=lambda h: h.replace('"', "~")):
         print("#include {}".format(header_needed))
+
     sys.stdout.write("\n")
     sys.stdout.flush()
 
